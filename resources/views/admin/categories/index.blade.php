3f2fadf59897dfe54d919ab04ff2fda2900e6a3c
Seongchan Kang <sckang511@hotmail.com>
2019-02-26 15:24:47 -0600

    initial commit

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skang
+ * Date: 2/26/2019
+ * Time: 1:53 PM
+ */
---
+++ resources/views/admin/meal/create.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skang
+ * Date: 2/26/2019
+ * Time: 2:09 PM
+ */
