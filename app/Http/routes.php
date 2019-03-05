82f433ab7ba4b9b52f31a73354531c16e3427142
Chris Loken <csoulman@hotmail.com>
2019-03-05 16:51:25 -0600

    test commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
-
+Route::get('/newpath', 'HomeController@index');
 Route::resource('admin/users', 'AdminUsersController');
 
 Route::get('/admin', function() {
